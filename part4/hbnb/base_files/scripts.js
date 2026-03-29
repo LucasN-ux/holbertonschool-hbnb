9938b11e9a60697cfcc4012313f7d57a3b266dc0
LucasN-ux <lucas.n230896@gmail.com>
2026-03-29 13:40:53 +0200

    fix: dynamic place

--- part4/hbnb/base_files/scripts.js
+++ part4/hbnb/base_files/scripts.js
@@ -11,7 +11,36 @@ function load(id, file) {
     });
 }
 
+function loadHeaderFooter(){
+    load("header", "header.html");
+    load("footer", "footer.html");
+}
+
+function initPlacesPage() {
+const container = document.getElementById("places-list");
+if (!container) return;
+
+// Exemple statique (plus tard API)
+const places = [
+    { name: "Place 1", price: 100 },
+    { name: "Place 2", price: 150 }
+];
+
+places.forEach(place => {
+    const card = document.createElement("article");
+    card.className = "place-card";
+
+    card.innerHTML = `
+    <h2>${place.name}</h2>
+    <p>Price: $${place.price}/night</p>
+    <a href="place.html?id=${place.id}">View Details</a>
+    `;
+
+    container.appendChild(card);
+});
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-  load("header", "header.html");
-  load("footer", "footer.html");
+    loadHeaderFooter();
+    initPlacesPage();
 });
